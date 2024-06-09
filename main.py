ee7d2a0ed78705736944c315baa9935f16c75a21
prakratichadda <chaddaprakrati@gmail.com>
2024-06-09 20:54:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 import streamlit as st
-openai.api_key=st.secrets["openai_api_key"]
+client = openai(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
