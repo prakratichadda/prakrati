75e88dc52a0c7ad292d051627a68fd541a09ce90
prakratichadda <chaddaprakrati@gmail.com>
2024-06-10 18:22:58 +0530

    main resume

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-import openai
+import asyncio
 import streamlit as st
-client = openai(api_key=st.secrets["OPENAI_API_KEY"])
+from ollama import AsyncClient
+from langchain_community.chat_models import ChatOllama
+from retrieval_grader import retrieve_and_grade
+from metadata_filter import metadata_filter
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+async def chat(content: str, message_placeholder):
+    """
+    Stream a chat from Llama using the AsyncClient.
+    """
+    llm = ChatOllama(model="llama3", format="json", temperature=0 , num_thread=16)
+    filter = metadata_filter(llm, content)
+    context = retrieve_and_grade(llm, content, filters=filter)
+    message = {
+        "role": "system",
+        "content": f"You are system to provide answers on the query {content} on basis of the context {context}",
+    }
+    response = ""
+    async for part in await AsyncClient().chat(
+        model="llama3", messages=[message], stream=True
+    ):
+        response += part["message"]["content"]
+        message_placeholder.markdown(response)
+    return response
+
+
+if "llama_model" not in st.session_state:
+    st.session_state["llama_model"] = "llama3"
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -22,13 +46,8 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        message_placeholder = st.empty()
+        response = asyncio.run(chat(prompt, message_placeholder))
+        message_placeholder.markdown(response)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
